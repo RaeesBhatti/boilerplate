e9652a60afd7fa6dd86d7df1cdd8d722a4044e5b
steelbrain <aneesiqbalbhatti@gmail.com>
2015-12-07 01:27:14 -0700

    :bug: Ignore any post data in main website

--- main.hh
+++ main.hh
@@ -2,4 +2,4 @@
 define('APP_ENV', 'MAIN');
 require(__DIR__.'/core/app-load.php');
 header('Content-Type: text/html; charset=utf-8');
-echo Helper::go(HTTP::GET, $_GET, $_POST, $_SERVER, $_COOKIE);
+echo Helper::go(HTTP::GET, $_GET, [], $_SERVER, $_COOKIE);
