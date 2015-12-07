4e06a83959b1e2573cfe59fd8602ec4c700cf499
steelbrain <aneesiqbalbhatti@gmail.com>
2015-12-06 20:29:13 -0700

    :new: Add Title, Header & Footer to Page

--- core/page.php
+++ core/page.php
@@ -1,5 +1,8 @@
 <?hh //strict
 abstract class Page {
+  public string $Title = '';
+  public array<Stringish> $Header = [];
+  public array<Stringish> $Footer = [];
   abstract public function __construct(): void;
   abstract public function render(): :html;
 }
