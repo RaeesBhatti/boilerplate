eeec16105c85ffef13b960087974753058a7da6f
steelbrain <aneesiqbalbhatti@gmail.com>
2015-12-07 01:41:18 -0700

    :art: Minor code nit

--- core/page.php
+++ core/page.php
@@ -3,6 +3,6 @@ abstract class Page {
   public string $Title = '';
   public array<Stringish> $Header = [];
   public array<Stringish> $Footer = [];
-  abstract public function __construct(): void;
+  abstract public function __construct();
   abstract public function render(): Stringish;
 }
