f031ad7b3529a5afa3210c59023516e512ac3d97
Raees Iqbal <raees.bhatti@gmail.com>
2015-12-13 20:55:47 +0500

    :+1: Allow nested arrays in API response

--- core/shapes.php
+++ core/shapes.php
@@ -1,6 +1,6 @@
 <?hh //strict
 type TRouterWeb = Router<classname<Page>>;
-type TRouterAPI = Router<(function():array<string, string>)>;
+type TRouterAPI = Router<(function():array<string, mixed>)>;
 type User = shape(
   'id' => int
 );
