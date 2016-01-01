da19bfcdb6021693fd43122976afd068bfdf419c
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-01 01:17:23 -0700

    :art: Fix credentials

--- credentials.empty.hh
+++ credentials.empty.hh
@@ -1,4 +1,5 @@
-<?hh //strict
+<?php //strict
+// ^ replace to <?hh when copied to credentials.hh
 const string CONFIG_DB_NAME = '';
 const string CONFIG_DB_USER = '';
 const string CONFIG_DB_PASS = '';
