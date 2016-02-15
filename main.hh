ded3d38d8cf5058d01133255b7af04e90d41010a
Raees Iqbal <raees.bhatti@gmail.com>
2016-02-16 01:13:36 +0500

    :fire: Remove Content-Type header from main.hh

--- main.hh
+++ main.hh
@@ -1,5 +1,4 @@
 <?php
 define('APP_ENV', 'MAIN');
 require(__DIR__.'/core/app-load.php');
-header('Content-Type: text/html; charset=utf-8');
 echo Helper::go(HTTP::GET, $_GET, [], $_SERVER, $_COOKIE);
