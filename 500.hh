71eb37f3f913db64cdda6304b22ba10821477bbd
Raees Iqbal <raees.bhatti@gmail.com>
2016-02-19 02:47:57 +0500

    Update 500 page

--- 500.hh
+++ 500.hh
@@ -1,3 +1,11 @@
 <?hh
 
-echo 'There was a problem with loading the page.';
+echo '<html>
+<head>
+<title>Error while page loading</title>
+<style>:root{font-family:sans-serif}</style>
+</head>
+<body>
+<p>There was a problem with loading the page. Please wait a few moments and try again</p>
+</body>
+</html>';
