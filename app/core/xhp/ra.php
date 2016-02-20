46c39ff779b2567129dfca2dc734065df696a454
Raees Iqbal <raees.bhatti@gmail.com>
2016-02-20 20:43:58 +0500

    Add https detection for ra

--- app/core/xhp/ra.php
+++ app/core/xhp/ra.php
@@ -2,7 +2,10 @@
 
 class :ra extends :a {
   public function stringify(): string {
-    $this->setAttribute('href', 'https://'.HOSTNAME.$this->getAttribute('href'));
+    $this->setAttribute('href', array_key_exists('HTTPS', App::getInstance()->Server) ?
+                                'https://'.HOSTNAME.$this->getAttribute('href') :
+                                '//'.HOSTNAME.$this->getAttribute('href')
+    );
     return parent::stringify();
   }
 }
