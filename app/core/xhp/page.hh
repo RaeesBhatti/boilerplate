90f55944dba3a14bc0fadea68418ea94da118b4a
Raees Bhatti <raees.bhatti@gmail.com>
2016-12-28 21:41:20 +0500

    Add default meta tags in <page>

--- app/core/xhp/page.hh
+++ app/core/xhp/page.hh
@@ -77,6 +77,8 @@ class :page extends :x:element {
 				<html lang={$this->:lang}>
 					<head>
 						<meta charset="UTF-8" />
+						<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+						<meta name="viewport" content="width=device-width, initial-scale=1" />
 						<title>{$Page->Title}</title>
 						{$Header}
 					</head>
