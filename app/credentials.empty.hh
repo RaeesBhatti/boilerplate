3bafbd3c8429d3145d6de9dbac0da4db4bc5c5f3
Raees Iqbal <10067728+elsteelbrain@users.noreply.github.com>
2017-12-24 12:54:19 +0500

    Config: Correct database addresses

--- app/credentials.empty.hh
+++ app/credentials.empty.hh
@@ -1,11 +1,11 @@
 <?php //strict
 // ^ replace to <?hh when copied to credentials.hh
-const string CONFIG_DB_HOST = 'localhost';
+const string CONFIG_DB_HOST = 'app-mongodb';
 const string CONFIG_DB_PORT = '27017';
 const string CONFIG_DB_NAME = 'app';
 const string CONFIG_DB_USER = '';
 const string CONFIG_DB_PASS = '';
-const string CONFIG_REDIS_HOST = 'localhost';
+const string CONFIG_REDIS_HOST = 'app-redis';
 const int CONFIG_REDIS_PORT = 6379;
 const string ASSETS_PREFIX = 'localhost/assets/';
 const string HOSTNAME = 'localhost';
